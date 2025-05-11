21c040e43678c888b72b94df7122eae3927abee3
Wilker Martins <dev.wilker@gmail.com>
2025-05-11 16:01:49 -0300

    ajuste no database tres

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://agendamento-on-front.vercel.app', // domínio do seu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],          // métodos permitidos
+  credentials: true                                    // se estiver usando cookies/autenticação
+}));
 app.use(express.json());
 
 // Conectar ao MongoDB
